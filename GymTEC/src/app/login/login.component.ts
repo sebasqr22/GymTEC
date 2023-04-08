82034213582db0ed36402996858d15d761022214
sebasqr22 <sebastianqr.2208@gmail.com>
2023-04-08 15:57:02 -0600

    Se agrega parte del sign in

--- GymTEC/src/app/login/login.component.ts
+++ GymTEC/src/app/login/login.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  mostrando = 0
   mostrar = false
+  ngOnInit() {
+    const signin = document.getElementById("signin") as HTMLInputElement
+    signin.style.display = 'none'
+  }
+
   mostrarContra(){
     const input = document.getElementById('floatingPassword') as HTMLInputElement
     if(this.mostrar){
@@ -16,5 +22,18 @@ export class LoginComponent {
       input.type = 'text'
     }
     this.mostrar = !this.mostrar
+  }
+
+  cambiarPantalla(){
+    const signin = document.getElementById("signin") as HTMLInputElement
+    const login = document.getElementById("login") as HTMLInputElement
+    if(this.mostrando === 0){
+      login.style.display = 'none'
+      signin.style.display = 'block'
+    }
+    else{
+      signin.style.display = 'none'
+      login.style.display = 'block'
+    }
   }
 }
