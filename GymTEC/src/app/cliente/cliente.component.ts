fe23c6d599837dee400bbaf899985d9c83aa26c4
sebasqr22 <sebastianqr.2208@gmail.com>
2023-04-10 12:14:13 -0600

    Pantalla Busqueda Vista Cliente

--- GymTEC/src/app/cliente/cliente.component.ts
+++ GymTEC/src/app/cliente/cliente.component.ts
@@ -11,7 +11,21 @@ export class ClienteComponent implements OnInit{
   dropdown = 0
 
   nombre = "Sebatian Quesada"
+  activo = 'principal'
   ngOnInit() {
+    const busqueda = document.getElementById("busqueda") as HTMLInputElement
+    const registro = document.getElementById("registro") as HTMLInputElement
+    busqueda.style.display = 'none'
+    registro.style.display = 'none'
+  }
+
+  desbloquear(vista:string){
+    const activo = document.getElementById(this.activo) as HTMLInputElement
+    activo.style.display = 'none'
+    //buscamos el nuevo
+    const nuevo = document.getElementById(vista) as HTMLInputElement
+    nuevo.style.display = 'block'
+    this.activo = vista
   }
 
   mostrarDropdown(){
@@ -24,5 +38,51 @@ export class ClienteComponent implements OnInit{
       drop.className = "dropdown-menu dropdown-menu-right shadow animated--grow-in"
       this.dropdown = 0
     }
+  }
+
+  cambiarPantalla(pantalla:string){
+    this.desbloquear(pantalla)
+  }
+
+  clase(tipo:string){
+    alert(tipo)
+  }
+
+  crearCartaClase(tipo:string, src:string, caption:string){
+    const carta = document.createElement('figure')
+    carta.className = 'cartaClase'
+    carta.addEventListener("click", () => {
+      this.clase(tipo);
+    });
+    const imagen = document.createElement('img');
+    imagen.src = src
+    carta.appendChild(imagen)
+
+    const figcap = document.createElement('figcaption')
+    figcap.innerHTML = caption
+    carta.appendChild(figcap)
+
+    return carta
+  }
+
+  buscarClases(){
+    const inicio = document.getElementById('fechaInicio') as HTMLInputElement
+    const final = document.getElementById('fechaFinaliza') as HTMLInputElement
+
+    if(inicio && final){
+      const fi = inicio.value
+      const ff = final.value
+      if(fi != "" && ff != ""){
+        const div = document.getElementById('clasesCartas') as HTMLInputElement
+        div.appendChild(this.crearCartaClase('natacion', 'https://img.freepik.com/premium-vector/continuous-line-drawing-young-professional-swimmer-practicing-swimming-indoor-pool-sports_500861-500.jpg', 'Natacion'))
+        div.appendChild(this.crearCartaClase('pilates', 'https://t3.ftcdn.net/jpg/03/72/43/12/360_F_372431283_rhVo7VWm2Fmgf2K0CBo99cbZuvfoCdau.jpg', 'Pilates'))
+      }
+      else{
+        alert("Los datos de Fecha de Inicio y Fecha de Finalización no deben estar vacíos!!")
+      }
+    }
+  }
+  logout(){
+    alert("logout")
   }
 }
