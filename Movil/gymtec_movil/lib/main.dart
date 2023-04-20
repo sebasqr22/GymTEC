ee6bf0bcd759375934bccec99015746bb53b6fe7
Justin Fernández-Badilla <jusfb18@gmail.com>
2023-04-20 09:05:01 -0600

    Scroll of the views

--- Movil/gymtec_movil/lib/main.dart
+++ Movil/gymtec_movil/lib/main.dart
@@ -29,7 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   late TextEditingController _controller;
   late TextEditingController _controller2;
 
@@ -64,10 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   "LOGO",
                   style: TextStyle(fontSize: 25),
                 ),
-                Text(
-                  "USER ICON",
-                  style: TextStyle(fontSize: 25),
-                ),
+                Icon(IconData(0xe043, fontFamily: 'MaterialIcons'),size:60),
                 Text(
                   "Correo electrónico: ",
                   style: TextStyle(fontSize: 25),
@@ -81,15 +77,173 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 TextField(
                   controller: _controller2,
+                  obscureText: true,
                 ),
                 ElevatedButton(
                   style: style,
-                  onPressed: () {},
+                  onPressed: () {
+                    _navigateToWelcome(context);
+                  },
                   child: const Text('Iniciar sesión'),
                 ),
                 ElevatedButton(
                   style: style,
-                  onPressed: () {},
+                  onPressed: () {
+                    _navigateToRegister(context);
+                  },
+                  child: const Text('Registrarse'),
+                ),
+              ],
+            ),
+            margin: const EdgeInsets.all(10.0),
+            width: 320.0,
+            height: 600.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(color: Colors.blue, spreadRadius: 3),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void _navigateToWelcome(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => RegisterScreen()));
+  }
+
+  void _navigateToRegister(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => RegisterScreen()));
+  }
+}
+
+
+class RegisterScreen  extends StatefulWidget {
+  const RegisterScreen ({super.key});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  // This class is the configuration for the state.
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+
+
+class _RegisterScreenState extends State<RegisterScreen>{
+  late TextEditingController _controller;
+  late TextEditingController _controller2;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller2 = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bienvenido a GymTec')),
+      body: Center(
+          child: Container(
+            child: ListView(
+              padding: EdgeInsets.all(20),
+              children: <Widget>[
+                Text(
+                  "LOGO",
+                  style: TextStyle(fontSize: 25),
+                ),
+                Icon(IconData(0xe043, fontFamily: 'MaterialIcons'),size:60),
+                Text(
+                  "Cédula: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller,
+                ),
+                Text(
+                  "Nombre: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller2,
+                  obscureText: true,
+                ),
+                Text(
+                  "Primer Aepllido: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller,
+                ),
+                Text(
+                  "Segundo Apellido: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller2,
+                  obscureText: true,
+                ),
+                Text(
+                  "Edad: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller,
+                ),
+                Text(
+                  "Fecha de Nacimiento: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller2,
+                  obscureText: true,
+                ),
+                Text(
+                  "Peso: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller,
+                ),
+                Text(
+                  "IMC: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller2,
+                  obscureText: true,
+                ),
+                Text(
+                  "Dirección: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller,
+                ),
+                Text(
+                  "Correo Electrónico: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller2,
+                  obscureText: true,
+                ),
+                Text(
+                  "Contraseña: ",
+                  style: TextStyle(fontSize: 25),
+                ),
+                TextField(
+                  controller: _controller,
+                ),
+                ElevatedButton(
+                  style: style,
+                  onPressed: () {
+                  },
                   child: const Text('Registrarse'),
                 ),
               ],
