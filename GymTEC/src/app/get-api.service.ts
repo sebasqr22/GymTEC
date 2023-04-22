fcb1eede54626c8f2350c9aa277393a3f50164b5
Jimena León <85376454+menaleon@users.noreply.github.com>
2023-04-22 11:36:50 -0600

    Mini Fix

--- GymTEC/src/app/get-api.service.ts
+++ GymTEC/src/app/get-api.service.ts
@@ -11,9 +11,9 @@ export class GetApiService {
   // LOGIN COMPONENT
   call_SignUpCliente(cedula:number, nombre:string, apellido1:string, apellido2:string, fechaNacimiento:string, peso:number, direccion:string, correo:string, contrasena:string){
     return this.http.post(`https://localhost:7194/usuarios/cliente/SignUpCliente?cedula=${cedula}&nombre=${nombre}&apellido1=${apellido1}&apellido2=${apellido2}&fechaNacimiento=${fechaNacimiento}&peso=${peso}&direccion=${direccion}&correoElectronico=${correo}&contrasena=${contrasena}`, {});
-  }
+  } 
 
-  call_LoginClient(cedula:number, contrasena:string){
+  call_LoginCliente(cedula:number, contrasena:string){
     return this.http.get(`https://localhost:7194/usuarios/cliente/LoginCliente?cedula=${cedula}&contrasena=${contrasena}`);
   }
 
