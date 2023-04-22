22e4ab1d8e3501c753f5d5cf4ca5f34ca342fb14
Justin Fernández-Badilla <jusfb18@gmail.com>
2023-04-22 12:16:18 -0600

    SQL lite inicialización

--- Movil/gymtec_movil/lib/sqlite_service.dart
+++ Movil/gymtec_movil/lib/sqlite_service.dart
@@ -1,5 +1,7 @@
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';class SqliteService {
+import 'package:path/path.dart';
+
+class SqliteService {
   Future<Database> initializeDB() async {
     String path = await getDatabasesPath();
     
@@ -7,8 +9,16 @@ import 'package:path/path.dart';class SqliteService {
       join(path, 'database.db'),
       onCreate: (database, version) async {
          await database.execute( 
-           "CREATE TABLE Notes(id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT NOT NULL)",
-      );
+           "CREATE TABLE CLIENTE (Cedula INT NOT NULL, Nombre NVARCHAR(50) NOT NULL,Apellido1 NVARCHAR(50),"+
+           "Apellido2 NVARCHAR(50),Dia_nacimiento VARCHAR(2) NOT NULL,Mes_nacimiento VARCHAR(2) NOT NULL,"+
+           "Año_nacimiento VARCHAR(4) NOT NULL,Peso FLOAT NOT NULL,Direccion NVARCHAR(50),"+
+           "Correo NVARCHAR(50) NOT NULL,Contraseña NVARCHAR(50) NOT NULL,PRIMARY KEY (Cedula));"+
+           "CREATE TABLE CLASE (Id_servicio INT NOT NULL,Num_clase INT NOT NULL,Fecha DATE,"+
+           "Hora_inicio TIME,Hora_fin TIME,Modalidad NVARCHAR(50),Capacidad INT,"+
+           "Cedula_instructor INT NOT NULL,PRIMARY KEY (Id_servicio, Num_clase));"+
+          "CREATE TABLE ASISTENCIA_CLASE (Cedula_cliente INT NOT NULL,Id_servicio INT NOT NULL,"+
+          "Num_clase INT NOT NULL,PRIMARY KEY (Cedula_cliente, Id_servicio, Num_clase));",
+      );// FALTAN LOS ALTER
      },
      version: 1,
     );
